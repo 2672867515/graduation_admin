df0887b8461ac2c316f848649396304115185e36
吴鹏辉 <2672867515@qq.com>
2024-02-13 10:43:55 +0800

    update

---
+++ project/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{Suspense, useEffect, useState} from "react";
+import { Route,Link,Switch,Redirect,useHistory  } from 'react-router-dom';
+import routes from './router.ts'
+function App() {
+  return (
+    <div className="App">
+          {/* 注册路由 */}
+      <Suspense>
+        <Switch>
+          {routes.map((item,index)=>{
+           return <Route path={item.path} key={index} component={item.component} />
+          })}
+          <Redirect to="/home" />
+        </Switch>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
